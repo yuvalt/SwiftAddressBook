9d86e7c9b7cbe1599ebed83c18f9d4a135b00a8a
Tassilo Karge <tassilo.karge@socialbit.de>
2015-03-21 16:25:46 +0100

    refactored and fixed bug in SwiftAddressBookSource (should fix #25)

--- Pod/Classes/SwiftAddressBookSource.swift
+++ Pod/Classes/SwiftAddressBookSource.swift
@@ -16,32 +16,26 @@ public class SwiftAddressBookSource : SwiftAddressBookRecord {
 
 	public var sourceType : SwiftAddressBookSourceType {
 		get {
-			let sourceType : CFNumber = ABRecordCopyValue(internalRecord, kABSourceTypeProperty).takeRetainedValue() as CFNumber
-			var rawSourceType : Int32? = nil
-			CFNumberGetValue(sourceType, CFNumberGetType(sourceType), &rawSourceType)
-			return SwiftAddressBookSourceType(abSourceType: rawSourceType!)
+			return SwiftAddressBookSourceType(abSourceType: internalSourceType)
 		}
 	}
 
 	public var searchable : Bool {
 		get {
-			let sourceType : CFNumber = ABRecordCopyValue(internalRecord, kABSourceTypeProperty).takeRetainedValue() as CFNumber
-			var rawSourceType : Int32? = nil
-			CFNumberGetValue(sourceType, CFNumberGetType(sourceType), &rawSourceType)
-			let andResult = kABSourceTypeSearchableMask & rawSourceType!
-			return andResult != 0
+			return (kABSourceTypeSearchableMask & internalSourceType) != 0
+		}
+	}
+
+	private var internalSourceType : Int32 {
+		get {
+			let sourceType = ABRecordCopyValue(internalRecord, kABSourceTypeProperty)?.takeRetainedValue() as NSNumber
+			return sourceType.intValue
 		}
 	}
 
 	public var sourceName : String? {
 		get {
-			let value: AnyObject? = ABRecordCopyValue(internalRecord, kABSourceNameProperty)?.takeRetainedValue()
-			if value != nil {
-				return value as CFString
-			}
-			else {
-				return nil
-			}
+			return ABRecordCopyValue(internalRecord, kABSourceNameProperty)?.takeRetainedValue() as? NSString
 		}
 	}
 }
